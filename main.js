5f85c048387f770c3c58f408f71d8b51ebf9a762
WinningValtry <75178947+WinningValtry@users.noreply.github.com>
2020-11-28 13:01:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function forgot() {
+    document.getElementById("oof").innerHTML = "I still don't know what to write so i don't see the meaning in trying to read this sooo I guess we have to say bye so then byeeeeeeeeeeeeeeee. Jus saying ya'll are all noobs.";
+}
